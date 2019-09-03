705bf880b3d69e1cd370696ea8c96105263f94d8
Barbara <bbrumfoto@gmail.com>
2019-09-02 21:55:29 -0300

    aula3

---
+++ exercicio.01/src/TestaCondicional.java
@@ -0,0 +1,24 @@
+
+public class TestaCondicional {
+
+	public static void main(String[] args) {
+		 System.out.println("Boate Kiss");
+			int idade = 18;
+		    int quantiPessoas =	3;
+		
+		    if (idade >= 18 ) {
+		    	System.out.println("Vc é maior de idade!");
+		        System.out.println("Seja bem-vindo Pode entrar!");
+		   
+		       if ( quantiPessoas == 3) {
+		    	   System.out.println("Vc está acompanhado dos pais!");
+		    	   System.out.println("Sejam bem-vindos!");
+		       }
+		    } else { 
+			      System.out.println("Vc não pode entrar !");
+		
+		    }		
+
+	}
+
+}
